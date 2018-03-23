136c68294d67113597de2e84a15fe1c06fb49f8d
Paul Davis <pauldavis@pitt.edu>
2018-03-23 17:47:11 -0400

    We have working buttons!

--- ScannerGUI/src/sample/Controller.java
+++ ScannerGUI/src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.GridPane;
+
+import java.util.concurrent.TimeUnit;
+
 public class Controller {
+
+    @FXML
+    Button easyCreate;
+    @FXML
+    Button b2;
+    @FXML
+    CheckBox easyCheck;
+    @FXML
+    ProgressBar easyLoading;
+
+    public void initialize() {
+        easyTab();
+    }
+
+    void easyTab(){
+
+        easyCreate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                easyCreate.setText("working!");
+            }
+        });
+
+        b2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                b2.setText("working!");
+            }
+        });
+
+
+
+
+    }
+
+
+
+
 }
