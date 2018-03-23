dc6920b6ddc19c64a31deeb994c9d1422ba2dc0a
Paul Davis <pauldavis@pitt.edu>
2018-03-23 16:45:13 -0400

    Trying to figure out fx

--- ScannerGUI/src/sample/Main.java
+++ ScannerGUI/src/sample/Main.java
@@ -1,20 +1,33 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("A Scanner Darkly");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+
+
+
+
+
     }
+
+
+
+
 
 
     public static void main(String[] args) {
