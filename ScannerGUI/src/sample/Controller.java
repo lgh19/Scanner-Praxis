d197b1e96a2a330469d12fc745914cada8105044
Paul Davis <pauldavis@pitt.edu>
2018-03-31 00:01:16 -0400

    More GUI Changes

--- ScannerGUI/src/sample/Controller.java
+++ ScannerGUI/src/sample/Controller.java
@@ -4,11 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 
-import javax.imageio.IIOException;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class Controller {
 
@@ -22,9 +19,9 @@ public class Controller {
 
     //Medium Tab
     @FXML
-    Button mediumImport;
+    Button mediumCreatePDF;
     @FXML
-    Button mediumProcess;
+    ChoiceBox mediumColorMode = new ChoiceBox();
 
     //Hard Tab
     @FXML
@@ -34,13 +31,42 @@ public class Controller {
     @FXML
     Button hardScan;
     @FXML
+    Button hardRunScanTailor;
+    @FXML
+    Button hardCreatePDF;
+    @FXML
     Spinner hardRotate;
+    @FXML
+    ChoiceBox hardLayoutOption = new ChoiceBox();
+    @FXML
+    ChoiceBox hardOrientation = new ChoiceBox();
+    @FXML
+    ChoiceBox hardColorMode = new ChoiceBox();
+
 
 
     public void initialize() {
+        setText();
+
         easyTab();
         mediumTab();
         hardTab();
+    }
+
+    public void setText(){
+        mediumColorMode.getItems().addAll("Black and White", "Color Grayscale", "Mixed");
+        mediumColorMode.setValue("Black and White");
+
+        hardRunScanTailor.setDisable(true);
+        hardCreatePDF.setDisable(true);
+        hardLayoutOption.getItems().addAll("Auto Detect", "One Page", "Two Page");
+        hardLayoutOption.setValue("Auto Detect");
+        hardOrientation.getItems().addAll("Left", "Right", "Upsidedown");
+        hardOrientation.setValue("Left");
+        hardRotate.setPromptText("0.0");
+        hardColorMode.getItems().addAll("Black and White", "Color Grayscale", "Mixed");
+        hardColorMode.setValue("Black and White");
+
     }
 
     void easyTab(){
@@ -48,23 +74,17 @@ public class Controller {
         easyCreate.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                easyCreate.setText("working!");
-                String[] cmd = new String[]{"/bin/sh", "path/to/script.sh"};
+                easyCreate.setText("Creating!");
+                //String[] cmd = new String[]{"/bin/sh", "path/to/script.sh"};
             }
         });
     }
 
     void mediumTab(){
-        mediumImport.setOnAction(new EventHandler<ActionEvent>() {
+        mediumCreatePDF.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                mediumImport.setText("working!");
-            }
-        });
-        mediumProcess.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                mediumProcess.setText("working!");
+                mediumCreatePDF.setText("Processing!");
             }
         });
     }
@@ -74,31 +94,47 @@ public class Controller {
             @Override
             public void handle(ActionEvent event) {
                 hardImport.setText("Importing!");
-                String[] env = {"PATH=/bin:/usr/bin/"};
-                String cmd = "you complete shell command";
                 try {
-                    Process process = Runtime.getRuntime().exec(cmd, env);
-                    Process pr = Runtime.getRuntime().exec(cmd);
+                    ProcessBuilder pb = new ProcessBuilder("sh test");
+                    pb.redirectErrorStream(true);
+                    pb.start();
+                    //StringBuilder out = new StringBuilder();
+                    //BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
                 }
                 catch (IOException e){System.out.println("Nope");}
             }
         });
-        ChoiceBox hardOrientation = new ChoiceBox();
-        hardOrientation.getItems().addAll("item1", "item2", "item3");
-
         hardDelete.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                hardDelete.setText("Deleting!");
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Are you sure you want to delete contents on camera??", ButtonType.YES, ButtonType.CANCEL);
+                alert.showAndWait();
+                if (alert.getResult() == ButtonType.YES) {
+                    hardDelete.setText("Deleting!");
+                }
             }
         });
         hardScan.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                hardScan.setText("Scanning!");
+                hardScan.setText("Processing!");
+                hardRunScanTailor.setDisable(false);
+                hardCreatePDF.setDisable(false);
             }
         });
-        hardRotate.setPromptText("0.0");
+        hardRunScanTailor.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                hardRunScanTailor.setText("Opening!");
+            }
+        });
+        hardCreatePDF.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                hardCreatePDF.setText("Creating!");
+            }
+        });
 
     }
 
